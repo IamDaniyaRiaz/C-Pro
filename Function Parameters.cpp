38a6e7f185f4bd01b331dd9fed22c474317574ed
Dania Riaz <104225110+IamDaniyaRiaz@users.noreply.github.com>
2022-06-04 03:34:39 -0700

    Create Function Parameters.cpp

---
+++ Function Parameters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void myFunction(string fname) {
+  cout << fname << "Hussain \n";
+}
+
+int main() {
+  myFunction("Asif");
+  myFunction("Ayaz");
+  myFunction("Seraj");
+  return 0;
+}
